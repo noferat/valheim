ceb9a087ff600bed3a392932c5b93f55a7850450
William Höglund Leuf <william.leuf@thingwave.eu>
2021-02-19 19:02:01 +0100

    2021-02-19@19-02-01,14

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 git pull
-start /W C:\WINDOWS\system32\mspaint.exe
-type hello > date.txt
+rem start /W C:\WINDOWS\system32\mspaint.exe
+
+start steam://rungameid/892970
 git commit -am "%DATE:/=-%@%TIME::=-%"
+git push
