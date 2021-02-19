840a2a66ce7a5f4bd36bb04856edbafb8da97267
William Höglund Leuf <william.leuf@thingwave.eu>
2021-02-19 19:22:56 +0100

    2021-02-19@19-22-56,36

--- start.bat
+++ start.bat
@@ -1,13 +1,30 @@
-git pull
-rem start /W C:\WINDOWS\system32\mspaint.exe
+rem Insert your specifics here
+set World_name = Jebsworld
+set save_folder = %USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds
 
+rem Get the newest world from git
+git pull
+
+rem Replace the world currently saved
+@echo Copying files...
+copy %World_name%.db %save_folder%\%world_name%.db
+copy %World_name%.fwl %save_folder%\%world_name%.fwl
+
+rem Start the game
 start /b /W steam://rungameid/892970
 
+rem Repeatedly check for game to stop
+@echo off
 :wait
 timeout 5 >nul
 tasklist | findstr /i /L "valheim.exe" > nul
 if not errorlevel 1 goto wait
 :QUIT
+
+@echo on
+@echo Saving files...
+copy %save_folder%\%world_name%.db %World_name%.db 
+copy %save_folder%\%world_name%.fwl %World_name%.fwl 
 
 git commit -am "%DATE:/=-%@%TIME::=-%"
 git push
